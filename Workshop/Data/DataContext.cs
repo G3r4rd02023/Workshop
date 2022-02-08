c7ca4626c97f3f071aeba3f1edf053e283d89edc
Laly Durón <LalyDOsorto@gmail.com>
2022-02-08 11:43:24 -0600

    data context

---
+++ Workshop/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Workshop.Data.Entities;
+
+namespace Workshop.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext>options) : base(options)
+        {
+
+        }
+
+        public DbSet<DocumentType> DocumentTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<DocumentType>().HasIndex(x => x.Description).IsUnique();
+        }
+    }
+}
--- Workshop/Program.cs
+++ Workshop/Program.cs
@@ -1,7 +1,16 @@
+
+using Microsoft.EntityFrameworkCore;
+using Workshop.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<DataContext>(x =>
+{
+    x.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
